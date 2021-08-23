c3fbdc6888e09d536f1c8a01c0e44dab8d8d3fe3
ttucker <tracytuckeremail@gmail.com>
2021-08-23 12:44:08 -0500

    Initial useState creation with 3 objects

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [state, setState] = useState(initialState{
+    "todo": {
+      title: "Todo",
+      items: []
+    },
+    "in-progress": {
+      title: "In Progress",
+      items: []
+    },
+    "done": {
+      title: "Completed",
+      items: []
+    }
+  })
   return (
     <div className="App">
       test
